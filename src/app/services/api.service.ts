fadb1d3acf9b092acd9b234c5b031320b32777bb
sneha sundaran mp <snehapallippuram@gmail.com>
2025-02-20 13:14:58 +0530

    up

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  server_url:String='https://recipesserver-n6i8.onrender.com/'
+  server_url:String='https://recipesserver-n6i8.onrender.com'
 
   constructor(private http:HttpClient) { }
 
